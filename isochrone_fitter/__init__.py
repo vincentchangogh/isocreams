5bab6d52d929f9ecd70033f94e394636b8fb5ff0
Chan Anand <chan.anand.98@gmail.com>
2023-09-27 21:04:03 +1000

    Added .dat files to data, and plotted age_7 isochrone using a .txt file

--- isochrone_fitter/__init__.py
+++ isochrone_fitter/__init__.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python
 """
-This is a module that is looking at a dataframe with the following variables:
-HIP, Vmag, Plx, B-V, SpType
-This is then used to create an HR diagram.
-
-References:
-    "http://balbuceosastropy.blogspot.com/2014/03/construction-of-hertzsprung-russell.html"
+This module aims to plot various isochrones using data from the Padova database.
 """
-%matplotlib inline
-
-from __future__ import division
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-filename = '../isochrone_fitter/I_239_selection.tsv'
+filename = '../data/iso_jc_z008s_age_7.txt'
 
-def load_stellar_lib(filename):
-    df = pd.read_table(filename, skiprows=52, sep=';', header=None, index_col=0,
-                       names = ['HIP', 'Vmag', 'Plx', 'B-V', 'SpType'],
-                       skipfooter=1, engine='python')
-    
-    df_clean = df.applymap(lambda x: np.nan if isinstance(x, basestring)
-                       and x.isspace() else x)
-    df_clean= df_clean.dropna()
-    df_clean['Vmag'] = df_clean['Vmag'].astype(np.float)
-    df_clean['Plx'] = df_clean['Plx'].astype(np.float)
-    df_clean['B-V'] = df_clean['B-V'].astype(np.float)
-    # Add a new column with the absolute magnitude
-    df_clean['M_V'] = df_clean['Vmag'] + 5 * np.log10(df_clean['Plx']/100.)
+df = pd.read_csv(filename, sep='\t') # Reads file into Panda dataframe
+df = df.dropna(axis=1, how='all') # Removes NaN column
+print(df.head())
+
+df["Mb_Mv"] = df["Mb"] - df["Mv"]
+
+df.plot.scatter(x="Mb_Mv", y="Mv", alpha=0.5)
+ax = plt.gca()
+ax.set_ylim(ax.get_ylim()[::-1])
+plt.show()
